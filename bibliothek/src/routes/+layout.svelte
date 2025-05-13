eec033e26339a73818b1fe5cb34aed216abede64
bingesam <bingesam@zhaw.students.ch>
2025-05-13 21:25:26 +0200

    navbar

--- bibliothek/src/routes/+layout.svelte
+++ bibliothek/src/routes/+layout.svelte
@@ -1,33 +1,44 @@
 <script>
-    let { children } = $props();
+  let { children } = $props();
 </script>
-
 
 <title>MiniBibliothek</title>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-black" style="padding-left: 130px;">
+<nav
+  class="navbar navbar-expand-lg navbar-dark bg-black"
+  style="padding-left: 130px;"
+>
   <div class="container-fluid">
     <a class="navbar-brand" href="/">MiniBibliothek</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNavAltMarkup"
+      aria-controls="navbarNavAltMarkup"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
         <a class="nav-link active" aria-current="page" href="/books">Bücher</a>
       </div>
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="/create">Hinzufügen</a>
+      </div>
     </div>
   </div>
 </nav>
 
 <div class="container">
-{@render children()}
+  {@render children()}
 </div>
-
 
 <style>
   :global(body) {
     background-color: black;
     color: white;
   }
-
 </style>
