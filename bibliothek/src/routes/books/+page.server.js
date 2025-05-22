f5a716550c35e382c83742a6489984134aed7418
bingesam <bingesam@zhaw.students.ch>
2025-05-22 16:16:14 +0200

    delete btn

--- bibliothek/src/routes/books/+page.server.js
+++ bibliothek/src/routes/books/+page.server.js
@@ -6,3 +6,11 @@ export async function load() {
   };
 }
 
+export const actions = {
+  delete: async ({ request }) => {
+    const data = await request.formData();
+    console.log(data.get("id"));
+    let id = data.get("id");
+    await db.deleteBook(id);
+  }
+};
--- bibliothek/src/routes/books/+page.svelte
+++ bibliothek/src/routes/books/+page.svelte
@@ -11,6 +11,10 @@
   {#each data.books as book}
     <div class="col-sm-6 col-md-4 col-lg-3 mb-2 gx-2">
       <BookCard book={book}/>
+<!--       <form action="?/delete" method="POST">
+        <input type="hidden" name="id" value={book._id}>
+        <button class="btn btn-danger">Delete</button>
+      </form> -->
     </div>
   {/each}
 </div>
