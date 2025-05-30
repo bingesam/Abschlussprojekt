ca420a18151cf641c3a9341fdbd689dd5817a46a
bingesam <bingesam@zhaw.students.ch>
2025-05-31 00:52:26 +0200

    undo

--- bibliothek/src/routes/books/+page.svelte
+++ bibliothek/src/routes/books/+page.svelte
@@ -1,39 +1,18 @@
 <script>
   import BookCard from "$lib/components/BookCard.svelte";
-
-  const { data } = $props();
-
-  let search = $state(""); // Suchtext (zustand)
-  
-  const filteredBooks = $derived.by(() =>
-    data.books.filter((book) =>
-      book.title.toLowerCase().includes(search.toLowerCase())
-    )
-  );
+  const {data} = $props();
 </script>
-
 
 <h3>Alle Bücher in deiner Bibliothek</h3>
 <p><i>Data is from Mockaroo, Images generated with ChatGPT and DALL-E</i></p>
 
-<input
-  class="form-control mb-4"
-  placeholder="Buchtitel durchsuchen..."
-  bind:value={$search}
-/>
-
-
 <div class="row">
-  {#each $filteredBooks as book}
+  {#each data.books as book}
     <div class="col-sm-6 col-md-4 col-lg-3 mb-2 gx-2">
       <BookCard book={book}/>
     </div>
   {/each}
 </div>
 
-
 <style>
-  input {
-    max-width: 400px;
-  }
 </style>
